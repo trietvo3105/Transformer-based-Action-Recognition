f2fa6456f6cccf6e162c8b222987fa48f326ab8d
Minh Triet VO <trietvo315@gmail.com>
2025-07-21 22:23:49 +0200

    [src:models] Add transformer-based pose classifier

--- src/models.py
+++ src/models.py
@@ -50,3 +50,68 @@ class FeatureExtractor(nn.Module):
     def forward(self, x):
         with torch.no_grad():
             return self.encoder(x)
+
+
+class VideoMAEPoseClassifier(nn.Module):
+    """
+    A custom Transformer-based classification architecture that uses the VideoMAE encoder followed by a layer-normalization layer and a linear layer to produce logits.
+    Args:
+        cnn_embedding_dim (int): Dimension of the CNN embeddings produced by the CNN feature extractor.
+        num_classes (int): Number of classes.
+        pretrained (bool): Whether to use a pretrained model.
+    """
+
+    def __init__(
+        self, cnn_embedding_dim: int, num_classes: int, pretrained: bool = True
+    ):
+        super().__init__()
+
+        if pretrained:
+            model_name = "MCG-NJU/videomae-base-finetuned-kinetics"
+            config = VideoMAEConfig.from_pretrained(model_name)
+            videomae_base = VideoMAEModel.from_pretrained(model_name)
+        else:
+            config = VideoMAEConfig()
+            videomae_base = VideoMAEModel(config)
+
+        self.position_embeddings = videomae_base.embeddings.position_embeddings
+        self.cls_token = nn.Parameter(torch.randn((1, 1, config.hidden_size)))
+
+        if cnn_embedding_dim != config.hidden_size:
+            self.projection = nn.Linear(cnn_embedding_dim, config.hidden_size)
+        else:
+            self.projection = nn.Identity()
+
+        self.encoder = videomae_base.encoder
+        self.fc_norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.classifier = nn.Linear(config.hidden_size, num_classes)
+
+    def forward(self, x):
+        """
+        Args:
+            x (torch.Tensor): A tensor of shape
+                (batch_size, num_frames, cnn_embedding_dim) containing the
+                features extracted by your CNN.
+        """
+        batch_size, num_frames, _ = x.shape
+        x = self.projection(x)  # (batch_size, num_frames, hidden_size)
+        cls_tokens = self.cls_token.expand(
+            batch_size, -1, -1
+        )  # (batch_size, 1, hidden_size)
+        x = torch.cat(
+            [cls_tokens, x], dim=1
+        )  # (batch_size, num_frames + 1, hidden_size)
+
+        if num_frames + 1 > self.position_embeddings.shape[1]:
+            raise ValueError(
+                f"The number of frames must be less than or equal to the size of the position embeddings: {self.position_embeddings.shape[1]}. "
+                f"Try to trim the video to have maximum of {self.position_embeddings.shape[1]} frames"
+            )
+
+        x = x + self.position_embeddings[:, : num_frames + 1].to(
+            x.device
+        )  # (batch_size, num_frames + 1, hidden_size)
+        x = self.encoder(x)[0]  # (batch_size, num_frames + 1, hidden_size)
+        x = self.fc_norm(x)  # (batch_size, num_frames + 1, hidden_size)
+        logits = self.classifier(x[:, 0])  # (batch_size, num_classes)
+        return logits
